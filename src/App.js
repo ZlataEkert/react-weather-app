757cb16e131a7964018d7309827831e370c6faaa
ZlataEkert <zlataekert@gmail.com>
2024-01-31 15:16:07 +0200

    Fetch the data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { ImSpinner9 } from "react-icons/im";
 
 const App = () => {
-  // 1. useState weather data const [data, setData] = useState()
-  // 2. fetch data handler (axios with api link, with apiToken)
-  // 3. useEffect with fetch data (city hardcode temporary)
+  const APIkey = "f4adf741a7b362beb26a9614ee5d1025";
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Kyiv");
+  const [loading, setLoading] = useState(false);
+
+  // Fetch the data
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}&units=metric`;
+    setLoading(true);
+    axios
+      .get(url)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [location]);
+
+  console.log(data);
+
   return (
-    <div>
-      <div>city name from useState</div>
-      <div>weather image from useState</div>
+    <div className="container">
+      <input className="input font-bold" placeholder="Enter City..." />
+      <div className="text-3xl">
+        Weather
+        {loading ? (
+          <ImSpinner9 className="text-9xl right-4 animate-spin" />
+        ) : (
+          data && (
+            <>
+              <div>City: {data.name}</div>
+              <div>Weather: {`${data.main.temp.toFixed()}℃`}</div>
+              <img
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt="ddd"
+              />
+            </>
+          )
+        )}
+        <div></div>
+      </div>
     </div>
   );
 };
